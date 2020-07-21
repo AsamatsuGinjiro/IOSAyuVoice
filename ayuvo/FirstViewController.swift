8c3945284f58d6ea73a5606c6d6de6f3a6c3bb28
AsamatsuGinjiro <aagin1973@gmail.com>
2020-07-21 15:35:34 +0900

    Initial Commit

---
+++ ayuvo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ayuvo
+//
+//  Created by Ginjiro Asamatsu on 2020/07/21.
+//  Copyright © 2020 Ginjiro Asamatsu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ayuvo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ayuvo
+//
+//  Created by Ginjiro Asamatsu on 2020/07/21.
+//  Copyright © 2020 Ginjiro Asamatsu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
