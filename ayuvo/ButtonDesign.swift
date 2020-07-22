68397509737f3aaff97d5e9fbd3fa8939a595051
AsamatsuGinjiro <aagin1973@gmail.com>
2020-07-22 17:38:02 +0900

    kaede,yowa

---
+++ ayuvo/ButtonDesign.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+@IBDesignable class RoundButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+    
+    func sharedInit() {
+        // Common logic goes here
+    }
+    func refreshCorners(value: CGFloat) {
+        layer.cornerRadius = value
+    }
+    @IBInspectable var cornerRadius: CGFloat = 15 {
+        didSet {
+            refreshCorners(value: cornerRadius)
+        }
+    }
+    func sharedInit() {
+        refreshCorners(value: cornerRadius)
+    }
+}
--- ayuvo/FirstViewController.swift
+++ ayuvo/FirstViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 import AVFoundation
 
 class FirstViewController: UIViewController {
-    @IBOutlet weak var button: UIButton!
     var audioPlayerInstance : AVAudioPlayer! = nil  // 再生するサウンドのインスタンス
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +17,9 @@ class FirstViewController: UIViewController {
                
     }
 
+    @IBAction func ButtonDesign(_ sender: Any) {
+        
+    }
     @IBAction func FirstScreenButton(_ sender: Any) {
         let soundFilePath = Bundle.main.path(forResource: "he", ofType: "mp3")!
         let sound:URL = URL(fileURLWithPath: soundFilePath)
@@ -51,8 +53,40 @@ class FirstViewController: UIViewController {
          audioPlayerInstance.currentTime = 0         // 再生箇所を頭に移す
          audioPlayerInstance.play()                  // 再生する
     }
+    @IBAction func FirstScreenButton3(_ sender: Any) {
+        let soundFilePath = Bundle.main.path(forResource: "kaede", ofType: "mp3")!
+        let sound:URL = URL(fileURLWithPath: soundFilePath)
+        // AVAudioPlayerのインスタンスを作成,ファイルの読み込み
+        do {
+            audioPlayerInstance = try AVAudioPlayer(contentsOf: sound, fileTypeHint:nil)
+        } catch {
+            print("AVAudioPlayerインスタンス作成でエラー")
+        }
+        // 再生準備
+        audioPlayerInstance.prepareToPlay()
+        // Do any additional setup after loading the view, typically from a nib.
+        // 連打した時に連続して音がなるようにする
+         audioPlayerInstance.currentTime = 0         // 再生箇所を頭に移す
+         audioPlayerInstance.play()                  // 再生する
+    }
+
     
-    
+    @IBAction func FirstScreenButtonyowa(_ sender: Any) {
+        let soundFilePath = Bundle.main.path(forResource: "yowa", ofType: "mp3")!
+        let sound:URL = URL(fileURLWithPath: soundFilePath)
+        // AVAudioPlayerのインスタンスを作成,ファイルの読み込み
+        do {
+            audioPlayerInstance = try AVAudioPlayer(contentsOf: sound, fileTypeHint:nil)
+        } catch {
+            print("AVAudioPlayerインスタンス作成でエラー")
+        }
+        // 再生準備
+        audioPlayerInstance.prepareToPlay()
+        // Do any additional setup after loading the view, typically from a nib.
+        // 連打した時に連続して音がなるようにする
+         audioPlayerInstance.currentTime = 0         // 再生箇所を頭に移す
+         audioPlayerInstance.play()                  // 再生する
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
